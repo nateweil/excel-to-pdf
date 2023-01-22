d434656e461b343a25fa13a7ba9cae8e912d4938
natew <nateweil1993@gmail.com>
2023-01-21 21:26:50 -0500

    Load data

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import pandas as pd
 import fpdf as pdf
+import glob
+import openpyxl
+
+#Will return a list object with all .xlsx files in the folder.
+filepaths = glob.glob("invoices/*.xlsx")
+
+
+for path in filepaths:
+    data = pd.read_excel(path, sheet_name="Sheet 1")
+    print(data)
